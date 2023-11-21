b009d55dba2579dfa7958edb8636886f22d89923
Sanjeevi <90151431+sanjeevi83@users.noreply.github.com>
2023-11-21 12:42:15 +0530

    init userlogin

--- adminlogin.php
+++ adminlogin.php
@@ -12,7 +12,7 @@ include "./inc/database.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="css\homemain.css">
-    <title>fsfgdfyughkjgfdsafghjkhgfdsfghjkkjhgfdsadfghjhgfd</title>
+    <title>kosika mathusi</title>
    <style>
 
     </style>
