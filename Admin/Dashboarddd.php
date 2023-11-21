07ac241353a37f7b2470f6b162666a992d660a87
M.Kosika <117499499+Kosi24@users.noreply.github.com>
2023-11-21 12:31:53 +0530

    Create Dashboarddd.php

---
+++ Admin/Dashboarddd.php
@@ -0,0 +1,126 @@
+<div class="sidebar close">
+    <div class="sidebar-brand">
+      <i class="fa-solid fa-book-open"></i>
+      <span class="logo_name">Book INforce</span>
+    </div>
+    <ul class="sidebar-menu ">
+      <li >
+        <a href="dashboard.php">
+          <i class="fa fa-th-large" ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="dashboard.php">Category</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="aprofile.php">
+          <i class='fa-solid fa-address-card' ></i>
+          <span class="link_name">profile</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">profile</a></li>
+        </ul>
+      </li>
+      <li>
+
+      <li>
+        <div class="title">
+          <a href="#">
+            <i class='fas fa-book' ></i>
+            <span class="link_name">Manageg Books</span>
+          </a>
+          <i class='fas fa-chevron-circle-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Manageg Books</a></li>
+          <li><a href="add_book.php">Add Book</a></li>
+          <li><a href="display_book.php">Display Book</a></li>
+          <li><a href="issue_book.php">Issue Book</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="issued_book.php">
+          <i class='fas fa-bookmark' ></i>
+          <span class="link_name">Issued Book</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="issued_book.php">Issued Book</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="requested_books.php">
+          <i class='fa-solid fa-user-pen' ></i>
+          <span class="link_name">Requested Book</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="requested_books.php">Requested Book</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="returned_book.php">
+          <i class='fas fa-reply' ></i>
+          <span class="link_name">Return Book</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="returned_book.php">Returned Books</a></li>
+        </ul>
+      </li>
+      
+      <li>
+        <a href="renew_book.php">
+          <i class='fas fa-recycle' ></i>
+          <span class="link_name">Renew Book</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="renew_book.php">Renew Book</a></li>
+        </ul>
+      </li>
+      
+      <li>
+        <a href="manage_user.php">
+          <i class='fas fa-user-cog' ></i>
+          <span class="link_name">Manage User</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="manage_user.php">Manage User</a></li>
+        </ul>
+      </li>
+      
+      <li>
+        <a href="fine_cal.php">
+          <i class='far fa-money-bill-alt' ></i>
+          <span class="link_name">Fine Calculator</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="fine_cal.php">Fine Calculator</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="manage_category.php">
+          <i class='fa-solid fa-bars-progress' ></i>
+          <span class="link_name">Book Categories</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="manage_category.php">Book Categories</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="../logout.php">
+          <i class='fa-solid fa-right-from-bracket' ></i>
+          <span class="link_name">Logout</span>
+        </a>
+
+      </li>  
+
+  
+    
+     
+</ul>
+</div>
