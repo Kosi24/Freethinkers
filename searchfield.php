ffc0f0d700e638919403d7b00daa08bab2aedc0a
mathus99 <117504283+mathus99@users.noreply.github.com>
2023-12-03 19:44:52 +0530

    Add files via upload

---
+++ searchfield.php
@@ -0,0 +1,7 @@
+<div class="search-wrapper">
+                <form action="book_list.php" method="post" >
+                    <input type="search" placeholder="Search Books" name="search" required/>
+                    <span class="fa fa-search"></span>
+                </form>
+                
+</div>
